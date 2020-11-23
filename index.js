d1761c00c310ccf819152845b87be6ebd1f851ac
mocno <gustavo.correa.mogi@gmail.com>
2020-11-22 23:36:06 -0300

    Início da criação do front-end, #1

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-// Chat_Papum v0.0.1
+// Chat_Papum v0.0.2
 
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const mongodb = require("mongodb");
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/css/*", (req, res) => {
+  res.sendFile(__dirname + req.url);
+});
+
+app.get("/js/*", (req, res) => {
+  res.sendFile(__dirname + req.url);
+});
+
+app.get("/text/*", (req, res) => {
+  res.sendFile(__dirname + req.url);
 });
 
 io.on("connection", (socket) => {
@@ -16,10 +29,10 @@ io.on("connection", (socket) => {
     console.log(`Usuario (${socket.id}) se desconectou`);
   });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("message", (msg) => {
+    io.emit("message", msg, socket.id, "Bruna");
 
-    console.log(`mensagem (${socket.id}): ${msg}`);
+    console.log(`mensagem de ${socket.id}: ${msg}`);
   });
 });
 
