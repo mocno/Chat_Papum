dd132ff1c0dab7dbceeb12868a80f813d35f1087
mocno <gustavo.correa.mogi@gmail.com>
2021-01-01 12:59:31 -0300

    Trocar os getElement's por querySelector's

--- front/js/chatpapum.js
+++ front/js/chatpapum.js
@@ -1,12 +1,12 @@
 var socket = io();
 
-document.getElementById("containerMessages").innerHTML =
+document.querySelector("#containerMessages").innerHTML =
   '<div class="messageHUD" id="messageHeader">Chat Papum:</div><ul id="messages"></ul><div class="messageHUD" id="messageInputs"><input id="messageText" autocomplete="off" placeholder="Digite uma mensagem" /><button id="messageSend" onclick="sendMessage();"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#bdd7ff" width="25px" height="25px"><path d="M0 0h24v24H0z" fill="none" /><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg></button></div>';
 
-document.getElementById("containerMessages").setAttribute("class", "");
+document.querySelector("#containerMessages").setAttribute("class", "");
 
-messageText = document.getElementById("messageText");
-messages = document.getElementById("messages");
+messageText = document.querySelector("#messageText");
+messages = document.querySelector("#messages");
 
 messageText.addEventListener("keydown", function (e) {
   if (e.which == 13) {
